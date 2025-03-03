ad7837a93ad91c74b35ebb698d57e1b352dc9d79
kostek437pja <s30393@pjwstk.edu.pl>
2025-03-03 13:18:05 +0100

    average

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Mod 1");
-Console.WriteLine("Mod 2");
-Console.WriteLine("Mod 3");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(average(arr));
+    }
+    public static int average(int[] arr)
+    {
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            count++;
+            sum += arr[i];
+        }
+
+        return sum / count;
+    }
+}
